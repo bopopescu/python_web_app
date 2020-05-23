8d2dd562c78f18b08d2d8c659a6c397936274404
youziss <2967661440@qq.com>
2020-05-23 21:39:01 +0800

    add coroweb

--- www/orm.py
+++ www/orm.py
@@ -254,7 +254,7 @@ class Model(dict, metaclass=ModelMetaclass):
         args = [self.getValue(self.__primary_key__)]
         rows = await execute(self.__delete__, args)
         if rows != 1:
-            logging.warn('failed to remove by primary key: affected rows: %s' % rows)
+            logging.warning('failed to remove by primary key: affected rows: %s' % rows)
 
 
 
