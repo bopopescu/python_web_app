28d228188c6a5e894461eb6da490140fee6ff306
youziss <2967661440@qq.com>
2020-05-28 13:07:11 +0800

    creat blog page

--- www/handlers.py
+++ www/handlers.py
@@ -1,12 +1,18 @@
 import asyncio
 from coroweb import get, post
-from models import User
+from models import User, Blog
+import time
 
 
 @get('/')
 async def index(request):
-    users = await User.findAll()
+    summary = 'Hello,World.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, create_at=time.time() - 120),
+        Blog(id='2', name='Something New', summary=summary, create_at=time.time() - 3600),
+        Blog(id='3', name='Learn Swift', summary=summary, create_at=time.time() - 7200)
+    ]
     return {
-        '__template__':'__base__.html',
-        'users':users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
